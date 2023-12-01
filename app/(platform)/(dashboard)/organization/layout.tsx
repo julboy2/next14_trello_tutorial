7f73bcff33012e31d7bd8974724c23a707ca0f91
JamesWoo <JamesWoo>
2023-12-01 16:42:02 +0900

    add Sidebar-1

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,13 +1,11 @@
-import { Navbar } from '../_components/navbar'
-
-export default function DashboardLayout({
+export default function OrganizationLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <div className="h-full">
-      <Navbar /> {children}
-    </div>
+    <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl ">
+      <div className="flex gap-x-7">{children}</div>
+    </main>
   )
 }
