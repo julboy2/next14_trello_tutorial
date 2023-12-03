abb8d5b5e12eac85ac8b04cab5680281471c8226
julboy2 <julboy2@gmail.com>
2023-12-03 22:32:25 +0900

    add sidebar -4

---
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,37 @@
+'use clinet'
+
+import { AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import { cn } from '@/lib/utils'
+
+export type Organization = {
+  id: string
+  slug: string
+  imageUrl: string
+  name: string
+}
+
+interface NavItemProps {
+  isExpanded: boolean
+  isActive: boolean
+  organization: Organization
+  onExpand: (id: string) => void
+}
+
+export function NavItem({
+  isExpanded,
+  isActive,
+  organization,
+  onExpand,
+}: NavItemProps) {
+  return (
+    <AccordionItem value={organization.id} className="border-none">
+      <AccordionTrigger
+        onClick={() => onExpand(organization.id)}
+        className={cn(
+          'flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline',
+          isActive && !isExpanded && 'bg-sky-500/10 text-sky-700'
+        )}
+      ></AccordionTrigger>
+    </AccordionItem>
+  )
+}
