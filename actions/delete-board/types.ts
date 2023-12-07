cbcb5fedf92aa8de4e66809cd3552f22499783f3
JamesWoo <JamesWoo>
2023-12-07 14:57:59 +0900

    finished board page

--- actions/delete-board/types.ts
+++ actions/delete-board/types.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { Board } from '@prisma/client'
 
 import { ActionState } from '@/lib/create-safe-action'
-import { UpdateBoard } from './schema'
+import { DeleteBoard } from './schema'
 
-export type InputType = z.infer<typeof UpdateBoard>
+export type InputType = z.infer<typeof DeleteBoard>
 export type ReturnType = ActionState<InputType, Board>
--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -4,15 +4,30 @@ import { FormInput } from '@/components/form/form-input'
 import { Button } from '@/components/ui/button'
 import { Board } from '@prisma/client'
 import { ElementRef, useRef, useState } from 'react'
+import { updateBoard } from '@/actions/update-board'
+import { useAction } from '@/hooks/use-action'
+import { toast } from 'sonner'
 
 interface BoardTitleFormProps {
   data: Board
 }
 
 export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board "${data.title}" updated!`)
+      setTitle(data.title)
+      disableEditing()
+    },
+    onError: (error) => {
+      toast.error(error)
+    },
+  })
+
   const formRef = useRef<ElementRef<'form'>>(null)
   const inputRef = useRef<ElementRef<'input'>>(null)
 
+  const [title, setTitle] = useState(data.title)
   const [isEditing, setIsEditing] = useState(false)
 
   const enableEditing = () => {
@@ -29,7 +44,11 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string
-    console.log('I am submitted', title)
+
+    execute({
+      title,
+      id: data.id,
+    })
   }
 
   const onBlur = () => {
@@ -47,7 +66,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
           ref={inputRef}
           id="title"
           onBlur={onBlur}
-          defaultValue={data.title}
+          defaultValue={title}
           className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
         ></FormInput>
       </form>
@@ -60,7 +79,7 @@ export const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
       variant={'transparent'}
       className="font-bole text-lg h-auto w-auto p-1 px-2"
     >
-      {data.title}
+      {title}
     </Button>
   )
 }
